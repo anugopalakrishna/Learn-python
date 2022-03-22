e90b82634da0391d0fb982674e4adcbea3a4ef01
anugopalakrishna <101447889+anugopalakrishna@users.noreply.github.com>
2022-03-22 21:01:05 +0530

    Add files via upload

---
+++ Day3/Project-1-number-word.py
@@ -0,0 +1,9 @@
+n=int(input("Enter any Number: "))
+if (n==1):
+      print("one")
+elif(n==2):
+     print("Two")
+elif(n==3):
+     print("Three")
+else:
+    print("invalid number")
---
+++ Day3/Project-3-Factorial.py
@@ -0,0 +1,18 @@
+n=5
+fact=1
+for i in range (1, n+1):
+      fact=fact*i
+      print("Factorial of",n," is",fact)
+      
+  
+n1=8
+fact1=1
+for i in range (1, n1+1):
+      fact1=fact1*i
+      print("Factorial of",n1," is",fact1)
+      
+n2=10
+fact2=1
+for i in range (1, n2+1):
+      fact2=fact2*i
+      print("Factorial of",n2," is",fact2)
